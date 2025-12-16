ae5eb437f9bb649af4f6e91197290b80636430c2
sarath-knackforge <sarathkumar.r@knackforge.com>
2025-12-16 16:59:11 +0530

    update contenrization code_1

--- ci/download_model.py
+++ ci/download_model.py
@@ -1,17 +1,19 @@
-import mlflow
 import os
+import mlflow
 
-# REQUIRED for Unity Catalog
-mlflow.set_registry_uri("databricks-uc")
+# REQUIRED for GitHub Actions + Unity Catalog
 mlflow.set_tracking_uri("databricks")
+mlflow.set_registry_uri("databricks-uc")
 
 model_name = os.environ["MODEL_NAME"]
 model_version = os.environ["MODEL_VERSION"]
 
 model_uri = f"models:/{model_name}/{model_version}"
 
+print(f"📦 Downloading model: {model_uri}")
+
 local_path = mlflow.artifacts.download_artifacts(
     artifact_uri=model_uri
 )
 
-print(f"✅ Model downloaded to {local_path}")
+print(f"✅ Model downloaded to: {local_path}")
