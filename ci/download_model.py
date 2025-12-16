4ac481bebd6d5d6bea9635c6a9956411ac4bc5bb
sarath-knackforge <sarathkumar.r@knackforge.com>
2025-12-16 18:56:13 +0530

    update to use containerize dev env in github

--- ci/download_model.py
+++ ci/download_model.py
@@ -1,23 +1,18 @@
 import os
 from databricks.sdk import WorkspaceClient
-from databricks.sdk.service import ml
 
 MODEL_NAME = os.environ["MODEL_NAME"]
 MODEL_VERSION = os.environ["MODEL_VERSION"]
 
 client = WorkspaceClient()
 
-# 1️⃣ Get model version info from Unity Catalog
 mv = client.model_versions.get(
     name=MODEL_NAME,
     version=MODEL_VERSION
 )
 
-# 2️⃣ Download model artifacts
-# This is the UC artifact location
 artifact_uri = mv.storage_location
-
-print(f"📦 Downloading model artifacts from: {artifact_uri}")
+print(f"📦 Downloading model from: {artifact_uri}")
 
 client.files.download(
     source_path=artifact_uri,
