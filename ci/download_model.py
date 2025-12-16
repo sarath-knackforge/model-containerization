b8908f6c62c576f31eb19ef45c2aa37da8b79d3a
sarath-knackforge <sarathkumar.r@knackforge.com>
2025-12-16 18:40:56 +0530

    update to use containerize dev env in github

--- ci/download_model.py
+++ ci/download_model.py
@@ -1,27 +1,19 @@
 import os
 import mlflow
 
-# --------------------------------------------------
-# CI ONLY: enable Databricks + Unity Catalog access
-# --------------------------------------------------
-mlflow.set_tracking_uri("databricks")
+# CI ONLY
 mlflow.set_registry_uri("databricks-uc")
 
-# Values passed from GitHub Actions
 model_name = os.environ["MODEL_NAME"]
 model_version = os.environ["MODEL_VERSION"]
 
-# Unity Catalog model URI
 model_uri = f"models:/{model_name}/{model_version}"
-
-# Local directory where model will be downloaded
-local_dir = "model"
 
 print(f"📦 Downloading model from {model_uri}")
 
 mlflow.artifacts.download_artifacts(
     artifact_uri=model_uri,
-    dst_path=local_dir
+    dst_path="model"
 )
 
-print(f"✅ Model downloaded locally to ./{local_dir}")
+print("✅ Model downloaded successfully")
